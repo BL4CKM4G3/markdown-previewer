c8bf864c7ebed9c47c7274b790f6ddff88f6f401
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-08-28 12:41:12 -0300

    feat: improving home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="heading">Markdown Previewer 1.0</h1>
+    <div class="d-flex row j-around">
+      <div class="d-flex col editor-wrapper">
+        <p class="caption">Editor</p>
+        <MonacoEditor
+          width="500"
+          height="80vh"
+          theme="vs-dark"
+          language="markdown"
+          :options="options"
+          v-model="text"
+        ></MonacoEditor>
+      </div>
+      <div class="d-flex col preview-wrapper">
+        <p class="caption">Preview</p>
+        <preview :markdown="text"></preview>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import MonacoEditor from 'monaco-editor-vue'
+import Preview from '../components/Preview.vue'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    MonacoEditor,
+    Preview
+  },
+  data: () => ({
+    text: '',
+    options: {
+
+    }
+  })
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  h1 {
+    text-align: center;
+  }
+}
+
+.editor-wrapper, .preview-wrapper {
+  padding-top: 25px;
+  p {
+    margin-bottom: 5px;
+  }
+}
+</style>
